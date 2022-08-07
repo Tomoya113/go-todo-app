fdd9959c3be31602dce4f75a18df1a020ff7e3fb
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-07 19:34:24 +0900

    [done] chapter14

---
+++ run.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func run(ctx context.Context, l net.Listener) error {
+	s := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+		}),
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.Serve(l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shudown: %+v", err)
+	}
+	return eg.Wait()
+}
