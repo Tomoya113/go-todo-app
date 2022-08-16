2c4fc8e5b4a1314caea464e95703efc181ccd0b7
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-17 00:31:32 +0900

    [add] run.go

--- run.go
+++ run.go
@@ -5,31 +5,25 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 
-	"golang.org/x/sync/errgroup"
+	"github.com/Tomoya113/go-todo-app/config"
 )
 
-func run(ctx context.Context, l net.Listener) error {
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
-		}),
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
 	}
 
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shudown: %+v", err)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
-	return eg.Wait()
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
