08a3924b846e787c5ac66941cd99d2ed7dc7038f
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-21 19:48:55 +0900

    [update] add_task handler

--- handler/add_task.go
+++ handler/add_task.go
@@ -3,15 +3,12 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
-	"github.com/Tomoya113/go-todo-app/entity"
-	"github.com/Tomoya113/go-todo-app/store"
 	"github.com/go-playground/validator/v10"
 )
 
 type AddTask struct {
-	Store     *store.TaskStore
+	Service   AddTaskService
 	Validator *validator.Validate
 }
 
@@ -34,13 +31,7 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t := &entity.Task{
-		Title:   b.Title,
-		Status:  entity.TaskStatusTodo,
-		Created: time.Now(),
-	}
-
-	id, err := store.Tasks.Add(t)
+	t, err := at.Service.AddTask(ctx, b.Title)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrResponse{
 			Message: err.Error(),
@@ -49,6 +40,6 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	rsp := struct {
 		ID int `json:"id"`
-	}{ID: id}
+	}{ID: int(t.ID)}
 	RespondJSON(ctx, w, rsp, http.StatusOK)
 }
